b57cb4d48f9633f0e7e9dada5838ef803bd5a9c8
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-06-20 17:34:34 -0300

    add display grid in strcture pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,16 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
-
+import './App.css';  // Importar o arquivo CSS
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
       <Home />
-      <Footer />  
-    </>
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
