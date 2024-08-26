2697eb8c200b080d5ec23a5ecd37f10e67aef313
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-08-25 22:07:42 -0300

    create user service

---
+++ investment/src/main/java/com/ferraz/investment/infra/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ferraz.investment.infra.service;
+
+import com.ferraz.investment.domain.entities.user.User;
+import java.util.UUID;
+import java.util.List;
+
+public interface UserService {
+    List<User> searchAll();
+
+    User searchById(UUID userId);
+
+    void insert(User user);
+
+    void update(UUID userId, User user);
+
+    void delete(UUID userId);
+}
