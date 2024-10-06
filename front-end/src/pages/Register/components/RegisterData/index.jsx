a957270be2cac6cd1e34072de2d682365a06884d
FelipeFerraz4 <felipeferraz2402@gmail.com>
2024-10-06 15:35:22 -0300

    fix: component register swap fix

--- front-end/src/pages/Register/components/RegisterData/index.jsx
+++ front-end/src/pages/Register/components/RegisterData/index.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import './style.css';
 import axios from 'axios';
 
-function LoginData() {
+import './style.css';
+
+function RegisterData() {
+    const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
@@ -14,43 +16,56 @@ function LoginData() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (email !== '' && password !== '') {
+        if (fullName !== '' && email !== '' && password !== '') {
             try {
-                const response = await axios.post('http://localhost:80/api/auth', {
+                const response = await axios.post('http://localhost:80/api/users', {
+                    name: fullName,
                     email: email,
                     password: password
                 });
 
-                if (response.status === 200) {
+                if (response.status === 201) {
                     navigate("/dashboard");
                     setError('');
                 }
             } catch (error) {
-                setError('E-mail ou senha inválido');
+                setError('Erro ao cadastrar o usuário');
             }
         } else {
-            setError('Campos E-mail ou senha vazio');
+            setError('Um dos campos está vazio');
         }
     };
 
-    return (
-        <section className="login">
-            <h3 className="login_title">Gerenciador de Investimentos</h3>
-            <h6 className="login_subtitle">Faça seu login e tenha um bom dia</h6>
+    return(
+        <section className="register">
+            <h3 className="register_title">Gerenciador de Investimentos</h3>
+            <h6 className="register_subtitle">Faça seu Cadastro e tenha um bom dia</h6>
+            
             <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="formEmail">
-                    <Form.Label className="login_form_title">E-mail</Form.Label>
+                <Form.Group controlId="formFullName">
+                    <Form.Label className="register_form_title">Nome Completo</Form.Label>
                     <Form.Control 
-                        type="email" 
-                        placeholder="Digite seu e-mail" 
+                        type="text" 
+                        placeholder="Digite seu nome completo" 
+                        value={fullName}
+                        onChange={(e) => setFullName(e.target.value)}
+                        required
+                        />
+                </Form.Group>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label className="register_form_title">E-mail</Form.Label>
+                    <Form.Control 
+                        type="email"
+                        placeholder="Digite seu e-mail"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         required
-                    />
+                        />
                 </Form.Group>
 
                 <Form.Group controlId="formPassword" className="mt-3">
-                    <Form.Label className="login_form_title">Senha</Form.Label>
+                    <Form.Label className="register_form_title">Senha</Form.Label>
                     <InputGroup className="password-input-group">
                         <FormControl
                             type={showPassword ? 'text' : 'password'}
@@ -58,20 +73,20 @@ function LoginData() {
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
-                        />
+                            />
                         <InputGroup.Text onClick={() => setShowPassword(!showPassword)} className="password-toggle-icon">
                             {showPassword ? <FaEyeSlash /> : <FaEye />}
                         </InputGroup.Text>
                     </InputGroup>
                 </Form.Group>
 
-                {error && <p className="login_error">{error}</p>}
+                {error && <p className="register_error">{error}</p>}
                 <Button variant="primary" type="submit" className="mt-3 w-100">
-                    Entrar
+                    Cadastrar
                 </Button>
             </Form>
         </section>
     );
 }
 
-export default LoginData;
+export default RegisterData;
